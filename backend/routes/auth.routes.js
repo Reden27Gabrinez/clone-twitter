4da5abb583ef8e785c94d86c687aaf6649390ef3
Reden27Gabrinez <redengabrinez1@gmail.com>
2024-08-18 00:50:07 +0800

    1-project setup

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { signup, login, logout } from "../controllers/auth.controller.js";
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/signup", login);
+router.post("/signup", logout);
+
+
+export default router;
